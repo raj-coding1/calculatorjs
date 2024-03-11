cf18c52c96aeb63a252e7b30c996f3251bed4a7b
raj-codinggit config --global user.name raj-coding1~ <rkgarai17@gmail.comgit config --global user.email rkgarai17@gmai>
2024-03-11 22:47:36 +0530

    commit

--- assingment/todoapplication/index.js
+++ assingment/todoapplication/index.js
@@ -1,1 +1,19 @@
 let a=document.createElement("h2")
+a.innerText="Item Name"
+a.setAttribute("class","heading1")
+let b=document.createElement("input")
+b.setAttribute("placeholder","enter item name")
+let c=document.createElement("h2")
+c.innerText="Quantity"
+c.setAttribute("class","heading2")
+let d=document.createElement("input")
+d.setAttribute("placeholder","enter quantity")
+let e=document.createElement("h2")
+e.innerText="Quantity"
+e.setAttribute("class","heading2")
+let g=document.createElement("input")
+g.setAttribute("placeholder","enter quantity")
+let f=document.querySelector("#container")
+let sub=document.createElement("button")
+sub.innerText="Submit"
+f.append(a,b,c,d,e,g,sub)
---
+++ assingment/todoapplication/index_todo.js
@@ -0,0 +1,9 @@
+for(let i=0;i<17;i++){
+    a=document.createElement("div") 
+    
+    a.innerText=i
+    
+    document.querySelector("#sub-container").append(a)
+
+
+}                                                                     
